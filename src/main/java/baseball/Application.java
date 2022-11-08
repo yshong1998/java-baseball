c478853ce10757788b284a7b44905c810a79952e
Ye Seok Hong <yshong1998@gmail.com>
2022-11-08 15:09:10 +0900

    docs : 두 번째 커밋 테스트

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,7 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("커밋 테스트입니다.");
+        System.out.println("두 번째 커밋 테스트입니다.");
 
     }
 }
