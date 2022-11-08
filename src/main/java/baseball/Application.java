b9ca0dd3df7c3deb6527a15a8bae7dd2283b865e
Ye Seok Hong <yshong1998@gmail.com>
2022-11-08 17:28:00 +0900

    feat : 어플리케이션 실행 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,13 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-
-
+        Stadium stadium = new Stadium();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (true) {
+            stadium.playGame();
+            if (stadium.user.restartNumber.equals("2")) {
+                break;
+            }
+        }
     }
 }
